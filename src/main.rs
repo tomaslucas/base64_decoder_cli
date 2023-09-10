4defc6c15473a7265796ccf6c2236b3ba140a6fb
Tomás Lucas <tomaslucas@gmail.com>
2023-09-10 18:52:37 +0200

    Mejoro presentación de información de help y pongo como opcional la búsqueda de palabras

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,20 @@ use clap::Parser;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
+#[command(before_help = "License MIT OR Apache-2.0")]
+#[command(name = "BASE64_DECODER_CLI")]
+#[command(version = "0.0.1")]
+#[command(author = "Tomás Lucas <tomaslucas@gmail.com>")]
+#[command(
+    about = "Decodifica una cadena en base64 desde un archivo y extrae una palabra específica"
+)]
+#[command(help_template("
+{before-help}{name} {version}
+{author-with-newline}{about-with-newline}
+{usage-heading} {usage}
+{all-args}{after-help}
+"))]
+#[command(after_help = "Esta es una prueba con CLAP.")]
 #[command(author, version, about, long_about = None)]
 //let m = command!().get_matches();
 /// Simple programa
@@ -24,9 +38,10 @@ struct Args {
         long,
         value_name("PALABRA"),
         help("Palabra a extraer de la salida decodificada"),
-        default_value_t = String::from("PRO"),
+        //default_value_t = String::from("PRO"),
+        
     )]
-    word: String,
+    word: Option<String>,
 }
 
 fn main() {
@@ -40,18 +55,18 @@ fn main() {
     println!("Contenido decodificado:\n{}", utf8_content);
 
     // Si se proporcionó el argumento 'word', extraer la palabra específica
-    if !args.word.is_empty() {
+    if args.word.is_some() {
         let extracted_word = utf8_content
             .split_whitespace()
-            .find(|&w| w == args.word)
+            .find(|&w| w == args.word.as_ref().unwrap())
             .unwrap_or("KO");
         if extracted_word == "KO" {
             println!(
                 "La palabra: {} -> NO se encuentra en el fichero.",
-                &args.word
+                &args.word.unwrap()
             );
         } else {
-            println!("La palabra: {} -> existe en el fichero.", &args.word);
+            println!("La palabra: {} -> existe en el fichero.", &args.word.unwrap());
         }
     }
 }
